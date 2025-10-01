e6cac513d0c0f171c1620cae1f73f8c3c09c04d2
yousafehsan-dev <yousafehsan125@gmail.com>
2025-10-02 04:53:33 +0500

    js change

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ let btn = document.getElementsByClassName("btn");
 // change img function 
 btn[0].onclick = function(){
     productimg.src = "img/front.png"
+    for(bt of btn ){
+        bt.classList.remove("active");
+     }
+     this.classList.add("active");
 }
 btn[1].onclick = function(){
     productimg.src = "img/side.png"
+     for(bt of btn ){
+        bt.classList.remove("active");
+     }
+     this.classList.add("active");
 }
+    
 btn[2].onclick = function(){
     productimg.src = "img/back.png"
+     for(bt of btn ){
+        bt.classList.remove("active");
+     }
+     this.classList.add("active");
 }
+
